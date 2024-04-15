991f1e5da0f48491a29efdf722d118370fb33460
ayush1108g <105841420+ayush1108g@users.noreply.github.com>
2024-04-16 00:11:58 +0530

    fix minor cors error

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,5 @@
-export const ToLink = "http://127.0.0.1:8000";
-// export const ToLink = "https://ecommerce-web-lwxy.onrender.com";
+// export const ToLink = "http://127.0.0.1:8000";
+export const ToLink = "https://ecommerce-web-lwxy.onrender.com";
 
 // export const ImageLink = "http://127.0.0.1:8000/image/";
 // export const ImageLink = "https://ecommerce-web-lwxy.onrender.com/image/";
